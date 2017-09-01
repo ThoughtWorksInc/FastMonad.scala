3f0d58465a1299a47daf2dd1e44be24f4546f468
Yang Bo <pop.atry@gmail.com>
2017-09-02 00:34:22 +0800

    WIP

--- fastmonad/src/main/scala/com/thoughtworks/fastmonad/Continuation.scala
+++ fastmonad/src/main/scala/com/thoughtworks/fastmonad/Continuation.scala
@@ -3,50 +3,59 @@ import scala.annotation.tailrec
 
 object Continuation {
 
-  abstract class IO[R <: Continuation[R, R], +A] extends Continuation[R, A] {
+  trait IO[State <: Continuation[State, State], +A] extends Continuation[State, A] {
     def result(): A
-    final def step(continue: A => Continuation[R, R]): Continuation[R, R] = {
+    final def step(continue: A => Continuation[State, State]): Continuation[State, State] = {
       continue(result())
     }
   }
 
   @tailrec
-  final def reset[R <: Continuation[R, R]](continue: Continuation[R, R]): R = {
+  final def reset[State <: Continuation[State, State]](continue: Continuation[State, State]): State = {
     continue match {
-      case io: IO[R, R] =>
+      case io: IO[State, State] =>
         io.result()
       case tailCall =>
-        reset(tailCall.step(identity[R]))
+        reset(tailCall.step(identity[State]))
     }
   }
 
-  trait NoState extends NoStateIO[NoState] {
+  object NoState {
+    private val Instance = new NoState
+
+    def apply(): NoState = Instance
+
+    type Continuation[+A] = com.thoughtworks.fastmonad.Continuation[NoState, A]
+
+    type IO[+A] = com.thoughtworks.fastmonad.Continuation.IO[NoState, A]
+  }
+
+  final case class NoState private[Continuation] () extends NoState.IO[NoState] {
     override def result(): NoState = this
   }
 
-  trait NoStateContinuation[+A] extends Continuation[NoState, A]
-
-  trait NoStateIO[+A] extends IO[NoState, A] with NoStateContinuation[A]
 }
 
-trait Continuation[R <: Continuation[R, R], +A] extends Any with Monad[A] {
+trait Continuation[State <: Continuation[State, State], +A] extends Any with Monad[A] {
 
-  type F[+A] = Continuation[R, A]
-  type G[+A] = Continuation[R, A]
+  type F[+A] = Continuation[State, A]
+  type G[+A] = Continuation[State, A]
 
-  final def run(continue: A => Continuation[R, R]): R = {
+  /** @usecase def onComplete(continue: A => State): State = ???
+    */
+  final def onComplete(continue: A => Continuation[State, State]): State = {
     Continuation.reset(step(continue))
   }
 
-  def map[B](f: (A) => B): Continuation[R, B] = { continueB =>
+  def map[B](f: (A) => B): Continuation[State, B] = { continueB =>
     step { a => continueR =>
       continueB(f(a))
     }
   }
 
-  def step(continue: A => Continuation[R, R]): Continuation[R, R]
+  def step(continue: A => Continuation[State, State]): Continuation[State, State]
 
-  def flatMap[B](f: A => Continuation[R, B]): Continuation[R, B] = { continue =>
+  def flatMap[B](f: A => Continuation[State, B]): Continuation[State, B] = { continue =>
     step { a =>
       f(a).flatMap(continue)
     }
